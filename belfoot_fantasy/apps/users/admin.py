659ebd8672440ecb251153ff1a32368eb88c26e1
root <root@bf13.by>
2024-12-23 19:47:15 +0300

    Серверные поправки

--- belfoot_fantasy/apps/users/admin.py
+++ belfoot_fantasy/apps/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import CustomUser
 # Register your models here.
+from django.views.decorators.csrf import csrf_exempt
 
-admin.site.register(CustomUser)
+csrf_exempt(admin.site.register(CustomUser))
