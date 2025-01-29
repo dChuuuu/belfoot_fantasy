f68988c6a9506b872358f91a3b065cb5c5391eb7
Chuuu <dustychuuu@gmail.com>
2025-01-29 21:14:05 +0300

    edited customuser model default email vailue

--- belfoot_fantasy/apps/users/models.py
+++ belfoot_fantasy/apps/users/models.py
@@ -152,7 +152,7 @@ class CustomUser(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = fields.GenericForeignKey('content_type', 'object_id')
-    email = models.EmailField(unique=True, default='none')
+    email = models.EmailField(unique=True, default='null')
 
 
     objects = CustomUserManager()
